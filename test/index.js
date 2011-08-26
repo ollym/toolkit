c5f27470f276982f685f17641a29a7d05a52e334
Oliver Morgan <oliver.morgan@kohark.com>
2011-08-26 20:21:22 +0100

    Finished all tests for Arrays

--- test/index.js
+++ test/index.js
@@ -3,15 +3,28 @@ require('../lib')
 
 var modules = ['Object', 'Array', 'Math'];
 var start = Date.now();
+var failures = 0;
 
 modules.forEach(function(mod) {
+  
   require('./test.' + mod).forEach(function(test) {
-    test(function(actual, expected, msg) {
+        
+    test(function(actual, expected, msg) {  
       if (actual != expected) {
+        failures++;
         console.error('\nFailure: ' + mod + '.' + test.name + '. Case "' + msg + '"\n\t' + 'Expected: "' + expected + '" Actual: "' + actual + '"');
       }
     });
   });
 });
 
-console.log('\nTests completed in: ' + (Date.now() - start) + 'ms');
+var elapsed = (Date.now() - start);
+
+if (failures == 0) {
+  console.log('\nAll Tests Passed!');
+}
+else {
+  console.log('\n' + failures + ' failures :(');
+}
+
+console.log('Tests completed in: ' + elapsed + 'ms');
--- test/test.Array.js
+++ test/test.Array.js
@@ -58,6 +58,64 @@ module.exports = [
     result(a.diff(b).join(','), 'd,a', 'Two-param Diff');
     result(a.diff(b, c).join(','), 'e,a', 'Three-param Diff');
     result([1, '2', '3'].diff(['1', 2, 3]).join(','), '1,2,3,1,2,3', 'Case-insentive diff');
+  },
+  
+  function Chunk(result) {
+    
+    result(Array.range(12).chunk(4).join(':'), '1,2,3,4:5,6,7,8:9,10,11,12', 'Even split');
+    result(Array.range(12).chunk(7).join(':'), '1,2,3,4,5,6,7:8,9,10,11,12', 'Odd split');
+    result(Array.range(12).chunk(13).join(':'), '1,2,3,4,5,6,7,8,9,10,11,12', 'No split');
+  },
+  
+  function Unique(result) {
+    
+    result(['a', 'a', 'b'].unique(), 'a,b', 'Normal');
+    result(['a', 'b', 'c'].unique(), 'a,b,c', 'No Change');
+  },
+  
+  function Each(result) {
+    
+    var j = 0;
+    var ret = [1, 2, 3].each(function(i) {
+      j = i;
+      if (i == 2) return 'foo';
+    });
+    
+    result(ret == 'foo', true, 'Return value');
+    result(j == 2, true, 'Stop looping');
+  },
+  
+  function Flatten(result) {
+    
+    result(JSON.stringify([1,2,[[3]]].flatten()), '[1,2,3]', 'Normal');
+    result(JSON.stringify([1,2,[[3]]].flatten(1)), '[1,2,[3]]', 'Level-1');
+    result(JSON.stringify([1,[2],[[[3]]]].flatten(2)), '[1,2,[3]]', 'Level-2');
+  },
+  
+  function Sum(result) {
+    
+    result([1, 2, 3].sum(), 6, 'Normal');
+  },
+  
+  function Product(result) {
+    
+    result([2, 3, 5].product(), 30, 'Normal');
+  },
+  
+  function First(result) {
+    
+    result([2, 3, 5].first(), 2, 'Normal');
+    result([2, 3, 5].first(2), '2,3', 'N-2');
+  },
+  
+  function Last(result) {
+    
+    result([2, 3, 5].last(), 5, 'Normal');
+    result([2, 3, 5].last(2), '3,5', 'N-2');
+  },
+  
+  function Clean(result) {
+    
+    result([undefined, null, false, 0, NaN, 'foo', 'bar'].clean(), 'foo,bar', 'Normal');
   }
-
 ];
