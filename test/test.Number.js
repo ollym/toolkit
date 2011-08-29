602d6deb0faf44e40bcf7e88f1c110f06bbd1a5b
Oliver Morgan <oliver.morgan@kohark.com>
2011-08-29 23:35:52 +0100

    Added Number.prototype.lcm. Ref #4

--- test/test.Number.js
+++ test/test.Number.js
@@ -4,6 +4,11 @@ module.exports = [
   function GCD(result) {
     
     result((12).gcd(6, 9), 3, 'Normal');
+  },
+  
+  function LCM(result) {
+    
+    result((21).gcd(6, 8), 42, 'Normal');
   }
 
 ];
