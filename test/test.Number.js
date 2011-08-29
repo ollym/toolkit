ede890a31eb1cad52d8f3bcd30e5c0afa8cc60e3
Oliver Morgan <oliver.morgan@kohark.com>
2011-08-30 00:13:41 +0100

    Made Number.prototype.[chr,even,odd] getters. Ref #4
    This makes a lot more sense to me. Any opinions?

--- test/test.Number.js
+++ test/test.Number.js
@@ -16,6 +16,23 @@ module.exports = [
     result((1.5).round(), 2, 'API Preservation');
     result((1).round(1).toString(), '1.0', 'Normal');
     result((15).round(-1), '20', 'Normal');
+  },
+  
+  function CHR(result) {
+    
+    result((72).chr, 'H', 'Getter');
+  },
+  
+  function Even(result) {
+    
+    result((3).even, false, 'Odd');
+    result((2).even, true, 'Even');
+  },
+  
+  function Odd(result) {
+    
+    result((3).odd, true, 'Odd');
+    result((2).odd, false, 'Even');
   }
 
 ];
