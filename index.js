b40355e4549fca6c739f9b914302f705c743d0c7
Oliver Morgan <oliver.morgan@kohark.com>
2011-09-09 04:27:12 +0100

    Added Arra.prototype self modification methods. Ref #13

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = { version: '1.2.0' };
+module.exports = { version: '1.3.0' };
 
 require('./lib/object');
 require('./lib/math');
--- test/test.Array.js
+++ test/test.Array.js
@@ -30,6 +30,16 @@ module.exports = [
     result(['a', 'b', 'c'].shuffle().contains('a', 'b', 'c'), true, 'Element preservation');
   },
   
+  function Shuffle$(result) {
+    
+    result(['a', 'b', 'c'].shuffle$().contains('a', 'b', 'c'), true, 'Element preservation');
+    
+    var a = ['a', 'b', 'c']
+    var b = a.shuffle$();
+    
+    result(a, b, 'Object equality');
+  },
+  
   function Clone(result) {
     
     var a = ['a', 'b'], b = a.clone();
@@ -63,6 +73,17 @@ module.exports = [
     result(Array.range(12).chunk(13).join(':'), '1,2,3,4,5,6,7,8,9,10,11,12', 'No split');
   },
   
+  function Chunk(result) {
+    
+    result(Array.range(12).chunk$(4).join(':'), '1,2,3,4:5,6,7,8:9,10,11,12', 'Even split');
+    result(Array.range(12).chunk$(7).join(':'), '1,2,3,4,5,6,7:8,9,10,11,12', 'Odd split');
+    result(Array.range(12).chunk$(13).join(':'), '1,2,3,4,5,6,7,8,9,10,11,12', 'No split');
+    
+    var a = Array.range(12), b = a.chunk$(4);
+    
+    result(a, b, 'Object equality');
+  },
+  
   function Unique(result) {
     
     result(['a', 'a', 'b'].unique(), 'a,b', 'Normal');
@@ -86,6 +107,17 @@ module.exports = [
     result(JSON.stringify([1,2,[[3]]].flatten()), '[1,2,3]', 'Normal');
     result(JSON.stringify([1,2,[[3]]].flatten(1)), '[1,2,[3]]', 'Level-1');
     result(JSON.stringify([1,[2],[[[3]]]].flatten(2)), '[1,2,[3]]', 'Level-2');
+  },
+  
+  function Flatten$(result) {
+    
+    result(JSON.stringify([1,2,[[3]]].flatten$()), '[1,2,3]', 'Normal');
+    result(JSON.stringify([1,2,[[3]]].flatten$(1)), '[1,2,[3]]', 'Level-1');
+    result(JSON.stringify([1,[2],[[[3]]]].flatten$(2)), '[1,2,[3]]', 'Level-2');
+    
+    var a = [1,2,[[3]]], b = a.flatten$();
+    
+    result(a, b, 'Object equality');
   },
   
   function Sum(result) {
@@ -113,5 +145,27 @@ module.exports = [
   function Clean(result) {
     
     result([undefined, null, false, 0, NaN, 'foo', 'bar'].clean(), 'foo,bar', 'Normal');
+  },
+  
+  function Clean$(result) {
+    
+    var arr = [undefined, null, false, 0, NaN, 'foo', 'bar'];
+    
+    result(arr.clean$(), 'foo,bar', 'Normal');
+    result(arr, 'foo,bar', 'Object Equality');
+  },
+  
+  function Filter$(result) {
+    var a = [1,2,3,4,5,6];
+    
+    result(a.filter$(function(n) { return n.even }), '2,4,6', 'Normal');
+    result(a, '2,4,6', 'Object equality');
+  },
+  
+  function Map$(result) {
+    var a = [1,2,3];
+    
+    result(a.map$(function(n) { return n.odd }), 'true,false,true', 'Normal');
+    result(a, 'true,false,true', 'Object equality');
   }
 ];
